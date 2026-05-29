ba7f48073481acd3233271f28b1b3aa8a2749c5b
Anthony Akio <anthony.akio2@gmail.com>
2026-05-29 18:45:44 -0300

    refactor(p2): atualiza consumo da api

--- src/services/countriesApi.js
+++ src/services/countriesApi.js
@@ -4,22 +4,22 @@ const api = axios.create({
   baseURL: "https://restcountries.com/v3.1",
 });
 
-export async function buscarPaisPorNome(nome) {
+export function buscarPaisPorNome(nome) {
   const termo = encodeURIComponent(nome.trim());
 
-  const resposta = await api.get(
-    `/name/${termo}?fields=name,translations,maps`
-  );
-
-  return resposta.data[0];
+  return api
+    .get(`/name/${termo}?fields=name,translations,maps`)
+    .then((resposta) => {
+      return resposta.data[0];
+    });
 }
 
-export async function buscarPaisPorCapital(capital) {
+export function buscarPaisPorCapital(capital) {
   const termo = encodeURIComponent(capital.trim());
 
-  const resposta = await api.get(
-    `/capital/${termo}?fields=name,flags`
-  );
-
-  return resposta.data[0];
+  return api
+    .get(`/capital/${termo}?fields=name,flags`)
+    .then((resposta) => {
+      return resposta.data[0];
+    });
 }
